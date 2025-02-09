e2a796b0ad44ce7052e22a604f33acb56b2d7183
Yudhaaaaa <yudhapratama11005@gmail.com>
2025-02-09 22:39:45 +0700

    add analytic

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "../components/footer";
+import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       >
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
