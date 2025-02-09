21e67ddf0d0bfbc636da666752fdf3904bcdb5f5
Yudhaaaaa <yudhapratama11005@gmail.com>
2025-02-09 23:06:22 +0700

    add redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: 'https://hendrian-yudha.vercel.app', // Subdomain baru
+          permanent: true, // Set ke true untuk redirect 301 (permanent redirect)
+        },
+      ]
+    },
+  }
+  
